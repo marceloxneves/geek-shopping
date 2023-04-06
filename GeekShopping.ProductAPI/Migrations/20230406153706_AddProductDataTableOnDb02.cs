d7832d3340457fe69861c22b752bfee8e41beb62
Marcelo <marceloxneves@gmail.com>
2023-04-06 15:55:13 -0300

    Commit inicial

---
+++ GeekShopping.ProductAPI/Migrations/20230406153706_AddProductDataTableOnDb02.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GeekShopping.ProductAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProductDataTableOnDb02 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "proce",
+                table: "product",
+                newName: "price");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "price",
+                table: "product",
+                newName: "proce");
+        }
+    }
+}
