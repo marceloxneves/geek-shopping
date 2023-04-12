e5c950ba5430d000bc3df00438a4a94c17144eb5
Marcelo <marceloxneves@gmail.com>
2023-04-12 19:10:35 -0300

    OrderAPI Implementation

---
+++ GeekShopping.OrderAPI/Repository/IOrderRepository.cs
@@ -0,0 +1,14 @@
+﻿
+using GeekShopping.OrderAPI.Model;
+
+namespace GeekShopping.OrderAPI.Repository
+{
+    public interface IOrderRepository
+    {
+        public interface IOrderRepository
+        {
+            Task<bool> AddOrder(OrderHeader header);
+            Task UpdateOrderPaymentStatus(long orderHeaderId, bool paid);
+        }
+    }
+}
